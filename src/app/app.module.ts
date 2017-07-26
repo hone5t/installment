75978e683470ddb1f9399d29a7e4a3ab49b0fdfe
hone5t <hone5t@hone5t.com>
2017-07-26 11:08:45 +0300

    remove contact page and add payments page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,10 @@
 import { NgModule, ErrorHandler } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
-import { MyApp } from './app.component';
+import { Installment } from './app.component';
 
 import { AboutPage } from '../pages/about/about';
-import { ContactPage } from '../pages/contact/contact';
+import { PaymentsPage } from '../pages/payments/payments';
 import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 
@@ -13,21 +13,21 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 
 @NgModule({
   declarations: [
-    MyApp,
+    Installment,
     AboutPage,
-    ContactPage,
+    PaymentsPage,
     HomePage,
     TabsPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(Installment)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
+    Installment,
     AboutPage,
-    ContactPage,
+    PaymentsPage,
     HomePage,
     TabsPage
   ],
--- src/pages/payments/payments.ts
+++ src/pages/payments/payments.ts
@@ -2,10 +2,10 @@ import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
 @Component({
-  selector: 'page-contact',
-  templateUrl: 'contact.html'
+  selector: 'page-payments',
+  templateUrl: 'payments.html'
 })
-export class ContactPage {
+export class PaymentsPage {
 
   constructor(public navCtrl: NavController) {
 
